519ae95f845945078c2fccff707ebd33db4b9265
kaleido9729 <chenkai9729@outlook.com>
2025-09-05 15:05:41 +0800

    Fix SSL certificate validation error in proxy

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://192.9.102.156:8443/jiermw-webapi",
-        changeOrigin: true
+        changeOrigin: true,
+        secure: false
       }
     }
   },
